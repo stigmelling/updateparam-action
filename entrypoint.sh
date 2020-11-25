dc3ee0c921f9e222542dd29b7a2185c51e3580da
Stig Melling <stig@kodemaker.no>
2020-11-25 10:19:51 +0100

    upd

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition "$TASK_FAMI
 NEW_TASK_DEFINTIION=$(echo $TASK_DEFINITION | jq --arg IMAGE "$ECR_IMAGE" '.taskDefinition | .containerDefinitions[0].image = $IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities)')
 NEW_TASK_INFO=$(aws ecs register-task-definition --region "$AWS_DEFAULT_REGION" --cli-input-json "$NEW_TASK_DEFINTIION")
 NEW_REVISION=$(echo $NEW_TASK_INFO | jq '.taskDefinition.revision')
-aws ecs update-service --cluster ${ECS_CLUSTER} \
-                       --service ${SERVICE_NAME} \
-                       --task-definition ${TASK_FAMILY}:${NEW_REVISION}
-aws ssm put-parameter --name ${SERVICE_NAME}-tag --type "String" --value ${IMAGE_TAG} --overwrite
+#aws ecs update-service --cluster ${ECS_CLUSTER} \
+#                       --service ${SERVICE_NAME} \
+#                       --task-definition ${TASK_FAMILY}:${NEW_REVISION}
+#aws ssm put-parameter --name ${SERVICE_NAME}-tag --type "String" --value ${IMAGE_TAG} --overwrite
